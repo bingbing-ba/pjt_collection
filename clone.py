87e015996201c156ed0aa1b5d7e4977492680779
eric <eric@hphk.kr>
2020-09-20 14:54:33 +0900

    Add oneliner for running the main server

--- clone.py
+++ clone.py
@@ -5,9 +5,22 @@ from utils import get_students
 BASE_URL = 'https://lab.ssafy.com'
 DIR_NAME = sys.argv[1]
 
+success = []
+failure = []
+
 for name, gitlab_name in get_students():
     print(f'========== {name} ==========')
-    # os.system(f'mkdir -p ./{DIR_NAME}/{name}')
-    os.system(
-        f'git clone {BASE_URL}/{gitlab_name}/{DIR_NAME}.git ./{DIR_NAME}/{name}')
+    result = os.system(f'git clone {BASE_URL}/{gitlab_name}/{DIR_NAME}.git ./{DIR_NAME}/{name}')
+    if result == 0:
+        success.append(name)
+        os.system(f'cd ./{DIR_NAME}/{name} && python manage.py migrate && cd ../..')
+    else:
+        failure.append(name)
+        os.system(f'rm -rf ./{DIR_NAME}/{name}')
     print()
+
+print((f'========== 총 {len(success)}명 완료 ✅ =========='))
+if len(failure) != 0:
+    print((f'========== 총 {len(failure)}명 실패 🚨 =========='))
+    for name in failure:
+        print(f'{name} 학생 저장소 확인 바랍니다.')
---
+++ pjts/management/commands/run_pjt.py
@@ -0,0 +1,61 @@
+from multiprocessing import Process
+import os
+import sys
+from pathlib import Path
+
+
+# 학생들 pjt에 X-Frame-Option관련 장고 default middleware 해제
+def deactivate_XFrameOption(target_pjt, pjt_dir):
+    students = os.listdir(pjt_dir)
+    students.sort()
+    students_with_port = [(student, idx) for idx, student in enumerate(students)]
+    
+    for student in students:
+        settings_file_url = pjt_dir / student / target_pjt / 'settings.py'
+        
+        new_lines = []
+        try:
+            with open(settings_file_url) as f:
+                lines = f.readlines()
+                for line in lines:
+                    if 'django.middleware.clickjacking.XFrameOptionsMiddleware' in line:
+                        continue
+                    new_lines.append(line)
+            with open(settings_file_url, 'w') as f:
+                f.writelines(new_lines)
+        except:
+            print(student, '프로젝트 구조에 문제가 있을 수 있음')
+    
+    return students, students_with_port
+
+
+def start_server(students_with_port, target_pjt, pjt_dir):
+    student, idx = students_with_port
+    student_pjt_dir = pjt_dir / student
+    os.chdir(student_pjt_dir)
+    port_num = str(idx).zfill(2)
+    os.system(
+        f'export DJANGO_SETTINGS_MODULE={target_pjt}.settings && python manage.py runserver 80{port_num}'
+    )
+
+
+def start_main_server():
+    os.system('python manage.py runserver 8080')
+
+
+def start_multiprocessing(target_pjt):
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+    pjt_dir = (Path(BASE_DIR) / target_pjt).absolute()
+
+    students, students_with_port = deactivate_XFrameOption(target_pjt, pjt_dir)
+
+    # multiprocessing
+    for idx in range(len(students)):
+        proc = Process(target=start_server, args=(students_with_port[idx], target_pjt, pjt_dir))
+        proc.start()
+    
+    # start the main server
+    proc = Process(target=start_main_server)
+    proc.start()
+
+
